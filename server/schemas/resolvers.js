4d03d56cdcf567cb2c6a652e1aec3a3ca26236cf
Krich2020 <keston@therichfamily.net>
2024-04-18 13:04:28 -0600

    fixed typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,36 +1,67 @@
-const { User } = require("../models");
+const { User, Movie } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate("characters");
+      return User.find().populate("saved_movies");
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate("characters");
+    user: async (parent, { email }) => {
+      return User.findOne({ email }).populate("saved_movies");
+    },
+    movies: async () => {
+      return Movie.find();
+    },
+    movie: async (parent, { imdb_id }) => {
+      return Movie.findOne({ imdb_id });
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, { email, password }) => {
+      const user = await User.create({ email, password });
       const token = signToken(user);
       return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw AuthenticationError;
+        throw new AuthenticationError("Incorrect email or password!");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw AuthenticationError;
+        throw new AuthenticationError("Incorrect email or password!");
       }
 
       const token = signToken(user);
       return { token, user };
+    },
+    addMovie: async (
+      parent,
+      { title, imdb_id, saved, banned, movie_poster }
+    ) => {
+      const movie = await Movie.create({
+        title,
+        imdb_id,
+        saved,
+        banned,
+        movie_poster,
+      });
+      return movie;
+    },
+    updateMovie: async (parent, { imdb_id, saved, banned }) => {
+      const updatedMovie = await Movie.findOneAndUpdate(
+        { imdb_id },
+        { saved, banned },
+        { new: true }
+      );
+      return updatedMovie;
+    },
+    deleteMovie: async (parent, { imdb_id }) => {
+      const deletedMovie = await Movie.findOneAndDelete({ imdb_id });
+      return deletedMovie;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,39 @@
 const typeDefs = `
+type Movie {
+  _id: ID!
+  title: String!
+  imdb_id: String!
+  saved: Boolean
+  banned: Boolean
+  movie_poster: String
+}
+
 type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    characters: [Character]!
+  _id: ID!
+  email: String!
+  password: String!
+  services: [String]
+  saved_movies: [Movie]
 }
 
 type Auth {
-    token: ID!
-    user: User
+  token: ID!
+  user: User
 }
 
 type Query {
-    users: [User]
-    user(username: String!): User
+  users: [User]
+  user(email: String!): User
+  movies: [Movie]
+  movie(imdb_id: String!): Movie
 }
 
 type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+  addUser(email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+  addMovie(title: String!, imdb_id: String!, saved: Boolean, banned: Boolean, movie_poster: String): Movie
+  updateMovie(imdb_id: String!, saved: Boolean, banned: Boolean): Movie
+  deleteMovie(imdb_id: String!): Movie
 `;
 
 module.exports = typeDefs;
