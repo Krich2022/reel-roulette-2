55950185112d2aaf2f24f3ce7e8a8b27a7f9278d
Krich2020 <keston@therichfamily.net>
2024-04-22 17:39:59 -0600

    added some styles for signup

--- client/src/pages/login-signup/signup.jsx
+++ client/src/pages/login-signup/signup.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Row, Col } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER_MUTATION } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [FormState, setFormState] = useState({ email: "", password: "" });
   const [addUser] = useMutation(ADD_USER_MUTATION);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
       variables: {
-        email: formState.email,
-        password: formState.password,
-        username: formState.username,
+        email: FormState.email,
+        password: FormState.password,
+        username: FormState.username,
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -24,49 +24,41 @@ function Signup(props) {
   const handleChange = (event) => {
     const { id, value } = event.target;
     setFormState({
-      ...formState,
+      ...FormState,
       [id]: value,
     });
   };
 
   return (
-    <div
-      direction="vertical"
-      className="container-background p-5 enable-rounded text-color"
-    >
+    <div className="d-flex justify-content-center">
       <h2 className="pb-3">Set Up Your Account</h2>
-
-      <form>
+      <Form>
         <div>
-          <Form.Control
-            type="input"
-            id="username"
-            name="username"
-            placeholder="username"
-            className="mb-4 input-container"
-            onChange={handleChange}
-          />
-          <Form.Control
-            type="input"
-            id="email"
-            name="email"
-            placeholder="email"
-            className="mb-4 input-container"
-            onChange={handleChange}
-          />
-          <Form.Control
-            type="password"
-            id="password"
-            name="password"
-            placeholder="password"
-            className="mb-3 input-container"
-            onChange={handleChange}
-          />
+          <Row>
+            <Form.Control
+              type="input"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className="mb-4 input-container"
+              onChange={handleChange}
+            />
+          </Row>
+          <Row>
+            <Form.Control
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className="mb-3 input-container"
+              onChange={handleChange}
+            />
+          </Row>
           <Button className="mb-3 button-container" onClick={handleSubmit}>
             Create Account
           </Button>
         </div>
-      </form>
+      </Form>
       <p className="text-lg">Already have an account?</p>
       <Button href="/login" className="mb-3 button-container">
         Login
